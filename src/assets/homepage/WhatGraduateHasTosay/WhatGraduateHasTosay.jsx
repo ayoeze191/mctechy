8204bd0117a89f29ee1179cfd477cfe3aa0275bd
ayoeze191 <ayoeze191@gmail.com>
2023-07-05 11:10:10 +0100

    asas

--- src/assets/homepage/WhatGraduateHasTosay/WhatGraduateHasTosay.jsx
+++ src/assets/homepage/WhatGraduateHasTosay/WhatGraduateHasTosay.jsx
@@ -17,8 +17,8 @@ const WhatGraduateHasTosay = () => {
             <div className='mb-[46.9px]'>
         <HompagesHeader text={width<768?"Hear from our graduates":'What graduates have to say'} after={'purple'}/>
         </div>
-        <div className='flex flex-col md:flex-row gap-[40px] md:gap-[132px]'>
-            <div className={`w-full md:w-1/2 ${width < 768?"order-2":"order-1"}`}>       
+        <div className='flex flex-col md:flex-row gap-[40px] md:gap-[132px] relative '>
+            <div className={`w-full md:w-1/2 ${width < 768?"order-2":"order-1"} sticky top-20 self-start `}>       
             <h2 className='font-bold text-[20px] leading-[26.4px] font-DmSansRegular text-grey md:text-text-black'>Featured Stories</h2>
             <p className='font-medium text-[16px] md:text-[24px] leading-[37px] md:leading-[47px]  font-DmSansRegular after:content after:content-[""] after:w-[85px] after:mt-[22px] md:after:mt-[23px] after:h-[11px] after:bg-purple after:rounded-[3px] after:block'>
             One of the difficulties that I had when I was studying web development on my own was that even though I could grasp the material and understand the concepts in an abstract format, I didn't always understand how it could be applied in real life. Getting the opportunity to learn from the Head Instructor for my course was really eye-opening for me.
